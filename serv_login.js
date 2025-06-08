433a601b6ba99de2154b6821a619e760b7c9a3f9
Jose Andres Calle Montes <joancamo2914@gmail.com>
2025-06-07 21:13:40 -0500

    nose bro

--- serv_login.js
+++ serv_login.js
@@ -1,25 +1,31 @@
 function cargarUsuarios () {
     const ws = ss.getSheetByName('usuarios');
     const credencialesList = ss.getRange("A2:C" + ws.getLastRow()).getValues();
-    Logger.log('Usuarios cargados: ' + credencialesList);
     return credencialesList;
 }
 
 function comprobarCredenciales (user, pass) {
+    Logger.log('Comprobando credenciales para el usuario: ' + user + ' con contraseña: ' + pass);
     const credencialesList = cargarUsuarios();
-    Logger.log(cargarUsuarios());
-    for (user of credencialesList) {
-        const usernameFound = user[0];
-        const passwordFound = user[1];
-        const rolFound = user[2];
+    for (claves of credencialesList) {
+        const usernameFound = claves[0];
+        Logger.log(claves[0]);
+        const passwordFound = claves[1];
+        Logger.log(claves[1]);
+        const rolFound = claves[2];
+        Logger.log(claves[2]);
         if (usernameFound === user && passwordFound === pass) {
-            Logger.log('Credenciales correctas para el usuario: ' + user);
+            Logger.log('retornando lo siguiente: ' + rolFound);
             return rolFound;
         }
     }
+    Logger.log('Credenciales incorrectas para el usuario: ' + user);
     return null;
 }
 
-function iniciarSesion () {
-
+function getScriptUrl() {
+  Logger.log('URL del script: ' + ScriptApp.getService().getUrl());
+    return ScriptApp.getService().getUrl();
+  
 }
+
